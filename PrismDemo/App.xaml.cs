b7381b1d5d0e351b928642dc07e4821d1cdd003f
leonleerl <leon.runlongli@gmail.com>
2024-10-30 22:02:05 +0800

    add RegionAdapter

---
+++ PrismDemo/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using PrismDemo.Core.Region;
+using PrismDemo.Views;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PrismDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<ShellWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+
+        protected override void ConfigureRegionAdapterMappings(RegionAdapterMappings regionAdapterMappings)
+        {
+            base.ConfigureRegionAdapterMappings(regionAdapterMappings);
+            regionAdapterMappings.RegisterMapping(typeof(StackPanel),
+                Container.Resolve<StackPanelRegionAdapter>());
+        }
+    }
+
+}
---
+++ PrismDemo/Core/Region/StackPanelRegionAdapter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PrismDemo.Core.Region
+{
+    public class StackPanelRegionAdapter : RegionAdapterBase<StackPanel>
+    {
+        public StackPanelRegionAdapter(RegionBehaviorFactory behaviorFactory) : base(behaviorFactory)
+        {
+            
+        }
+        protected override void Adapt(IRegion region, StackPanel regionTarget)
+        {
+            region.Views.CollectionChanged += (o, e) =>
+            {
+                if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+                {
+                    foreach (FrameworkElement item in e.NewItems)
+                    {
+                        regionTarget.Children.Add(item);
+                    }
+                }
+                else if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+                {
+                    foreach (FrameworkElement item in e.OldItems)
+                    {
+                        regionTarget.Children.Remove(item);
+                    }
+                }
+            };
+        }
+
+        protected override IRegion CreateRegion()
+        {
+            return new Region();
+        }
+    }
+}
